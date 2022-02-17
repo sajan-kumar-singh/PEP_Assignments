39229aa339a231507334cbc01ac5ec58055035df
Sajan Kumar Singh <saajan.199923@gmail.com>
2022-02-17 19:40:59 +0530

    First Assignment

---
+++ Session2_Assignment1.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class Session2_Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="\u0938\u0964\u091C\u0928";
+		System.out.println(s);
+
+	}
+
+}
